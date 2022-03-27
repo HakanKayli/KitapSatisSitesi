5c09d610ca8c228a5bcb55f513e3c65d646a545a
hakan2 <hakan2@172.18.248.153>
2022-03-27 15:24:18 +0300

    ilk yükleme

---
+++ src/java/util/DBConnection.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author hakan2
+ */
+public class DBConnection {
+    public Connection connet(){
+        Connection c = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager.getConnection("jdbc:postgresql://localhost:5433/kitapDatabase", "postgres", "98753");
+        } catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }   
+        return c;
+    }
+    
+}
